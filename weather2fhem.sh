3891ec4ab467368a2bc2044a5ba9076672e0877c
Gergely Budai <gbudai76@gmail.com>
2015-07-10 16:01:00 +0200

    Added auriol devices to fhem script.

--- weather2fhem.sh
+++ weather2fhem.sh
@@ -3,18 +3,46 @@
 HOST=localhost
 PORT=7072
 
-while read sensortype housecode channel status battery humid temp; do
+while read -a data; do
   batt_txt="ok"
   batt_stat=""
   hum_prefix=""
   warnings="none"
-  if [ $battery -eq 1 ]; then
-    batt_txt="low"
-    batt_stat=", Bat.low!"
-    warnings="battery"
+  device=""
+  temp=""
+  humid=""
+
+  case ${data[0]} in
+    wt440h)
+      device="${data[0]}_${data[1]}_${data[2]}"
+      temp="${data[6]}"
+      humid="${data[5]}"
+      if [ ${data[4]} -eq 1 ]; then
+        batt_txt="low"
+        batt_stat=", Bat.low!"
+        warnings="battery"
+      fi
+      if [ ${data[5]} -le 14 ]; then
+        hum_prefix="<="
+      fi
+    ;;
+
+    auriol)
+      device="${data[0]}_${data[1]}"
+      temp="${data[5]}"
+      humid="${data[6]}"
+      if [ ${data[2]} -eq 1 ]; then
+        batt_txt="low"
+        batt_stat=", Bat.low!"
+        warnings="battery"
+      fi
+#      if [ ${data[5]} -le 14 ]; then
+#        hum_prefix="<="
+#      fi
+    ;;
+  esac
+
+  if [ -n "${device}" ]; then
+    echo -ne "setreading ${device} temperature ${temp}\nsetreading ${device} humidity ${humid}\nsetreading ${device} battery ${batt_txt}\nsetreading ${device} warnings ${warnings}\nset ${device} ${temp}°C, ${hum_prefix}${humid}%${batt_stat}\n" | nc ${HOST} ${PORT}
   fi
-  if [ $humid -le 14 ]; then
-    hum_prefix="<="
-  fi
-  echo -ne "setreading ${sensortype}_${housecode}_${channel} temperature ${temp}\nsetreading wt440h_${housecode}_${channel} humidity ${humid}\nsetreading wt440h_${housecode}_${channel} battery ${batt_txt}\nsetreading wt440h_${housecode}_${channel} warnings ${warnings}\nset wt440h_${housecode}_${channel} ${temp}°C, ${hum_prefix}${humid}%${batt_stat}\n" | nc ${HOST} ${PORT}
 done
