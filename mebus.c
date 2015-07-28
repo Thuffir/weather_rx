27c52b3b76aaa19c6c5506a7496d7274e643bfb1
Vankuche <vankuche@web.de>
2015-07-28 06:51:30 +0200

    Added support for Mebus weather sensor.

---
+++ mebus.c
@@ -0,0 +1,196 @@
+/***********************************************************************************************************************
+ *
+ * Wireless Weather Station Receiver / Decoder for Raspberry Pi
+ *
+ * (C) 2015 Gergely Budai, Jens Hoffmann
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ *
+ **********************************************************************************************************************/
+
+#include "config.h"
+#ifdef MODULE_MEBUS_ENABLE
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/time.h>
+#include "types.h"
+#include "DecodePulseSpace.h"
+
+#ifndef ANALOG_FILTER
+
+// Pulse length in us
+#define PULSE_LENGTH       500
+// Space length for bit ZERO in us
+#define ZERO_LENGTH       1000
+// Space length for bit ONE in us
+#define ONE_LENGTH        2000
+
+#else // ANALOG_FILTER
+// The Analog filter alters the pulse / space timings
+
+// Pulse length in us
+#define PULSE_LENGTH       662
+// Space length for bit ZERO in us
+#define ZERO_LENGTH        780
+// Space length for bit ONE in us
+#define ONE_LENGTH        1850
+
+#endif // ANALOG_FILTER
+
+// Signal timing tolerance
+#define TOLERANCE          200
+
+// Suppress identical messages within this timeframe in uS
+#define SUPPRESS_TIME     1000000
+
+// Decoded data
+typedef struct {
+  uint8_t id;
+  uint8_t status;
+  uint16_t temperature;
+  uint8_t humidity;
+  uint32_t timeStamp;
+} MebusData;
+
+/***********************************************************************************************************************
+ * Mebus Message Decoder
+ **********************************************************************************************************************/
+static bool MebusDecode(MebusData *data, BitType bit)
+{
+  // Bit number counter
+  static uint8_t bitNr = 0;
+  // Return value
+  bool retval = false;
+  // Recheck some bits
+  bool reCheck;
+
+  // Only process valid bits
+  if(!(bit & BIT_VALID)) {
+    goto exit;
+  }
+
+  do {
+    // Only Recheck once
+    reCheck = false;
+
+    // Clear all data at the beginning
+    if(bitNr == 0) {
+      memset(data, 0, sizeof(MebusData));
+    }
+    else {
+      // All bits except the first must be in a bit stream
+      if(!(bit & BIT_IN_STREAM)) {
+//        printf("Bit not in stream: %u\n", bitNr);
+        bitNr = 0;
+        // Check again this bit, maybe it's the start of a new telegram
+        reCheck = true;
+        continue;
+      }
+    }
+    // Remove flags
+    bit &= BIT_ONE;
+
+    // ID [0 .. 13]
+    if(bitNr <= 13) {
+      data->id = (data->id << 1) | bit;
+    }
+    // Temperature [14 .. 23]
+    else if((bitNr >= 14) && (bitNr <= 23)) {
+      data->temperature = (data->temperature << 1) | bit;
+    }
+    // Status [24 .. 28]
+    else if((bitNr >= 24) && (bitNr <= 28)) {
+      data->status = (data->status << 1) | bit;
+    }
+    // Humidity [29..35]
+    else if((bitNr >= 29) && (bitNr <= 35)) {
+      data->humidity = (data->humidity << 1) | bit;
+    }
+
+    // Check if we have received everything
+    if(bitNr == 35) {
+      // Record reception Timestamp
+      struct timeval tv;
+      gettimeofday(&tv, NULL);
+      data->timeStamp = (tv.tv_sec * 1000000) + tv.tv_usec;
+      retval = true;
+    }
+
+
+    // Increment bit pointer
+    bitNr++;
+    // But not more than 36 Bits
+    if(bitNr > 36) {
+      bitNr = 0;
+    }
+  } while(reCheck);
+
+  exit:
+  return retval;
+}
+
+/***********************************************************************************************************************
+ * Process Bits for Mebus YD8220B
+ **********************************************************************************************************************/
+void MebusProcess(uint32_t pulseLength)
+{
+  // Bit decoder context
+  static PulseSpaceContext bitDecoderCtx = {
+    .pulseMin = PULSE_LENGTH - TOLERANCE,
+    .pulseMax = PULSE_LENGTH + TOLERANCE,
+    .zeroMin  = ZERO_LENGTH  - TOLERANCE,
+    .zeroMax  = ZERO_LENGTH  + TOLERANCE,
+    .oneMin   = ONE_LENGTH   - TOLERANCE,
+    .oneMax   = ONE_LENGTH   + TOLERANCE,
+    .state = Idle,
+    .inStream = 0
+  };
+  // Decoded data and the previous one
+  static MebusData data, prevData = { 0 };
+
+  // Decode Messages
+  if(MebusDecode(&data, DecodePulseSpace(&bitDecoderCtx, pulseLength))) {
+    // Check if a message is a duplicate of a last one
+    if((data.id != prevData.id) || (data.status != prevData.status) ||
+        (data.temperature != prevData.temperature) ||
+        (data.humidity != prevData.humidity) ||
+        ((data.timeStamp - prevData.timeStamp) >= SUPPRESS_TIME)) {
+      // No duplicate, convert temperature
+      double temperature;
+      temperature = data.temperature / 10.0;
+      // And Print
+      printf("mebus %u %u %.1f %u\n",data.id, data.status, temperature, data.humidity);
+      fflush(stdout);
+    }
+    // Remember old message
+    prevData = data;
+  }
+}
+
+#endif // MODULE_MEBUS_ENABLE
--- mebus.h
+++ mebus.h
@@ -2,7 +2,7 @@
  *
  * Wireless Weather Station Receiver / Decoder for Raspberry Pi
  *
- * (C) 2015 Gergely Budai
+ * (C) 2015 Gergely Budai, Jens Hoffmann
  *
  * This is free and unencumbered software released into the public domain.
  *
@@ -31,58 +31,18 @@
  *
  **********************************************************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#ifndef MEBUS_H_
+#define MEBUS_H_
+
+#include "config.h"
+#ifdef MODULE_MEBUS_ENABLE
 
 #include "types.h"
-#include "wt440h.h"
-#include "auriol.h"
-#include "rf_tech.h"
-#include "mebus.h"
 
-// LIRC device file
-#define LIRC_DEV          "/dev/lirc0"
-// Pulse length bits in lirc data
-#define LIRC_LENGTH_MASK  0xFFFFFF
+void MebusProcess(uint32_t pulseLength);
 
-/***********************************************************************************************************************
- * Main
- **********************************************************************************************************************/
-int main(void)
-{
-  // LIRC Device file descriptor
-  int lircDev;
-  // Data from lirc driver
-  uint32_t lircData;
+#else // MODULE_MEBUS_ENABLE
+#define MebusProcess(x)
+#endif // MODULE_MEBUS_ENABLE
 
-  // Open device file for reading
-  lircDev = open(LIRC_DEV, O_RDONLY);
-  if(lircDev == -1) {
-    perror("open()");
-    exit(EXIT_FAILURE);
-  }
-
-  // Receive and decode messages
-  while(1) {
-    // Wait and read data from lirc
-    if(read(lircDev, &lircData, sizeof(lircData)) != sizeof(lircData)) {
-      perror("read()");
-      exit(EXIT_FAILURE);
-    }
-    // Leave only the pulse length information
-    lircData &= LIRC_LENGTH_MASK;
-
-    // WT440H Messages
-    WT440hProcess(lircData);
-    // Auriol Messages
-    AuriolProcess(lircData);
-    // Mebus Messages
-    MebusProcess(lircData);
-    // RF-Tech Messages
-    RFTechProcess(lircData);
-  }
-
-  return 0;
-}
+#endif // MEBUS_H_
